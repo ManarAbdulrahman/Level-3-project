1b8f521de78afdc86cfb04401e55ca98dd97e3af
Ubuntu <ubuntu@ip-10-0-1-98.me-south-1.compute.internal>
2020-10-29 20:32:07 +0000

    everythin was ruined with port no.

--- e2e-js-test/checkout_test.js
+++ e2e-js-test/checkout_test.js
@@ -6,7 +6,7 @@
 
   casper.test.begin("User buys some socks", 7, function(test) {
     // initial load and login
-    casper.start("http://front-end:30001", function() {
+    casper.start("http://front-end/", function() {
      test.assertNotVisible("#login-modal", "user does not see the login dialogue");
      
      this.clickLabel("Login");
