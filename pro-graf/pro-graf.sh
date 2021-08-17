3044c358dc434ff8e31109a1934ca0cd67625587
ManarAbdulrahman <66467286+ManarAbdulrahman@users.noreply.github.com>
2021-08-17 21:36:36 +0300

    Update pro-graf.sh
    added sudo

--- pro-graf/pro-graf.sh
+++ pro-graf/pro-graf.sh
@@ -2,5 +2,5 @@
 helm repo add stable https://kubernetes-charts.storage.googleapis.com
 helm repo update
 helm install prometheus-operator stable/prometheus-operator --namespace monitor --set grafana.service.type=LoadBalancer
-kubectl apply -f ingress.yaml -n monitor
-kubectl get svc -n monitor | grep prometheus-operator-grafana
+sudo kubectl apply -f ingress.yaml -n monitor
+sudo kubectl get svc -n monitor | grep prometheus-operator-grafana
