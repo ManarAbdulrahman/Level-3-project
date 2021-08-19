7f267c29d000c5c9a35e84f133cca9d4315ed4dc
ManarAbdulrahman <66467286+ManarAbdulrahman@users.noreply.github.com>
2021-08-19 13:54:15 +0300

    Update pro-graf.sh
    changing helm chart link

--- pro-graf/pro-graf.sh
+++ pro-graf/pro-graf.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-helm repo add stable https://kubernetes-charts.storage.googleapis.com
+helm repo add stable https://charts.helm.sh/stable
 helm repo update
-helm install prometheus-operator stable/prometheus-operator --namespace monitor --set grafana.service.type=LoadBalancer
+sudo helm install prometheus-operator stable/prometheus-operator --namespace monitor --set grafana.service.type=LoadBalancer
 sudo kubectl apply -f ingress.yaml -n monitor
 sudo kubectl get svc -n monitor | grep prometheus-operator-grafana
